5fe87cb9a7436e50973990d65800a79d79d393d1
akshya <akshyathapa23@gmail.com>
2026-01-27 22:59:21 +0545

    deploy error fix 1

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { Toaster } from 'sonner';
 import UserLayout from './components/Layout/UserLayout';
 import Home from './components/Pages/Home';
-import Login from './components/Pages/login';
+import Login from './components/Pages/Login';
 import Register from './components/Pages/Register';
 import ScrollToTop from './components/Common/ScrollToTop';
 
